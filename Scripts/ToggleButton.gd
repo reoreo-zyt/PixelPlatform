1649c7d6c46ff06b7bb7e296114822cce96b8c42
reoreo <57691895+reoreo-zyt@users.noreply.github.com>
2022-12-12 02:34:11 +0800

    使用 AtalsTexture 管理图片素材及交互

--- Scripts/ToggleButton.gd
+++ Scripts/ToggleButton.gd
@@ -1,9 +1,11 @@
 extends Control
 
+# TODO: 有没有更好的拿路径的方法，支持实时更改路径？这种方式拿取路径当节点改变时就会导致无法同步更新，难以维护
 # 拿到动画
 # 拿路径可以直接将节点拖拽到脚本里
 onready var anim_player = $"../../../../../AnimationPlayer"
 onready var sidebar = $"../../.."
+onready var content = $Content
 
 # 事件
 func _on_ToggleButton_gui_input(event):
@@ -11,5 +13,6 @@ func _on_ToggleButton_gui_input(event):
 		if event.is_pressed():
 			if sidebar.rect_min_size[0] == 70:
 				anim_player.play("sidebar_anim")
+				content.texture.region = Rect2(165, 166, 22, 22)
 			else:
 				anim_player.play_backwards("sidebar_anim")
